7796a0f976e8959e378d03d54eb4533d77bbcdb6
Patryk <patrykokpara@hotmail.com>
2023-03-05 11:44:19 +0000

    Update to the script

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://chat-p.onrender.com', {
+    const response = await fetch('https://chat-p-back.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
